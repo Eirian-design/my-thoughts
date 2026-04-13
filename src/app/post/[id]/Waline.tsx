b8056ead7a690d28bbafea718c8731bd44137b0f
Eirian <eirian@EiriandeMacBook-Air.local>
2026-04-13 17:25:40 +0800

    fix: 换回 Commentbox，使用 lazyOnload

--- src/app/post/[id]/Waline.tsx
+++ src/app/post/[id]/Waline.tsx
@@ -1,27 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function Comments() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://utteranc.es/client.js';
-    script.setAttribute('repo', 'Eirian-design/my-thoughts');
-    script.setAttribute('issue-term', 'pathname');
-    script.setAttribute('label', 'comment');
-    script.setAttribute('theme', 'github-dark');
-    script.setAttribute('crossorigin', 'anonymous');
-    script.async = true;
-    
-    const container = document.getElementById('comments');
-    if (container) {
-      container.appendChild(script);
-    }
-  }, []);
-
   return (
     <div className="mt-10 pt-6" style={{ borderTop: '1px solid #333' }}>
-      <div id="comments"></div>
+      <div className="commentbox"></div>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/commentbox.io/dist/commentBox.min.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          // @ts-ignore
+          window.commentBox("5671822670430208-proj");
+        }}
+      />
     </div>
   );
 }
