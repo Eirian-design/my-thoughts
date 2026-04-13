f6cb9a2b0e8184ed8a511feea82776c0af554330
Eirian <eirian@EiriandeMacBook-Air.local>
2026-04-13 16:46:07 +0800

    fix: 修复 Commentbox 正确调用方式

--- src/app/post/[id]/Waline.tsx
+++ src/app/post/[id]/Waline.tsx
@@ -1,35 +1,28 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
 
 export default function Comments() {
+  useEffect(() => {
+    // @ts-ignore
+    if (window.commentBox) {
+      // @ts-ignore
+      window.commentBox("5671822670430208-proj", {
+        theme: "dark",
+        backgroundColor: "transparent",
+        textColor: "#e5e5e5",
+      });
+    }
+  }, []);
+
   return (
     <>
       <div className="mt-10 pt-6" style={{ borderTop: '1px solid #333' }}>
-        <div id="commentbox" className="comment-box"></div>
+        <div id="commentbox" className="commentbox"></div>
       </div>
-      <Script
-        id="commentbox-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var commentbox_settings = {
-              // 这里填你的 project ID，注册后获取
-              projectID: "5671822670430208-proj",
-              theme: "dark",
-              backgroundColor: "transparent",
-              textColor: "#e5e5e5",
-            };
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s);
-              js.id = id;
-              js.src = "https://cdn.jsdelivr.net/npm/commentbox.io/dist/commentBox.min.js";
-              fjs.parentNode.insertBefore(js, fjs);
-            })(document, "script", "commentbox-script");
-          `,
-        }}
+      <script
+        src="https://cdn.jsdelivr.net/npm/commentbox.io/dist/commentBox.min.js"
+        async
       />
     </>
   );
