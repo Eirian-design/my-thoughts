538e1794bcd5a557224da70d51cb787744081338
Eirian <eirian@EiriandeMacBook-Air.local>
2026-04-13 17:00:38 +0800

    fix: 换用 Utterances 评论系统

--- src/app/post/[id]/Waline.tsx
+++ src/app/post/[id]/Waline.tsx
@@ -6,18 +6,19 @@ export default function Comments() {
   return (
     <>
       <div className="mt-10 pt-6" style={{ borderTop: '1px solid #333' }}>
-        <div id="commentbox" className="commentbox"></div>
+        <div id="comments"></div>
       </div>
       <Script
-        src="https://cdn.jsdelivr.net/npm/commentbox.io/dist/commentBox.min.js"
+        src="https://utteranc.es/client.js"
         strategy="afterInteractive"
         onLoad={() => {
           // @ts-ignore
-          window.commentBox("5671822670430208-proj", {
-            theme: "dark",
-            backgroundColor: "transparent",
-            textColor: "#e5e5e5",
-          });
+          window.utterances = {
+            repo: "Eirian-design/my-thoughts",
+            issueTerm: "pathname",
+            label: "comment",
+            theme: "github-dark",
+          };
         }}
       />
     </>
