85abca14aa4dc205e15c19ef8b967e655e1076b2
Eirian <eirian@EiriandeMacBook-Air.local>
2026-04-13 16:52:02 +0800

    fix: 使用 Script 组件 onLoad 回调加载 Commentbox

--- src/app/post/[id]/Waline.tsx
+++ src/app/post/[id]/Waline.tsx
@@ -1,28 +1,24 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function Comments() {
-  useEffect(() => {
-    // @ts-ignore
-    if (window.commentBox) {
-      // @ts-ignore
-      window.commentBox("5671822670430208-proj", {
-        theme: "dark",
-        backgroundColor: "transparent",
-        textColor: "#e5e5e5",
-      });
-    }
-  }, []);
-
   return (
     <>
       <div className="mt-10 pt-6" style={{ borderTop: '1px solid #333' }}>
         <div id="commentbox" className="commentbox"></div>
       </div>
-      <script
+      <Script
         src="https://cdn.jsdelivr.net/npm/commentbox.io/dist/commentBox.min.js"
-        async
+        strategy="afterInteractive"
+        onLoad={() => {
+          // @ts-ignore
+          window.commentBox("5671822670430208-proj", {
+            theme: "dark",
+            backgroundColor: "transparent",
+            textColor: "#e5e5e5",
+          });
+        }}
       />
     </>
   );
